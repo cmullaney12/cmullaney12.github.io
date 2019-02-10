fede344c993f93a15e55af72943614943fc8381d
cmullaney12 <cmullaney12@cox.net>
2019-02-10 11:57:35 -0500

    more touch ups, mobile menu testing on contact page

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
+function toggleMobileMenu() {
+    var x = document.getElementById("mobileMenu");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
+
+$(document).ready(function(){
+	$('#burger-menu').click(function(){
+        $(this).toggleClass('open');
+        toggleMobileMenu();        
+	});
+});
+
 $(document).ready(function(){
   $('.painting-carousel').slick({
         dots: true,
@@ -28,29 +44,5 @@ $(document).ready(function(){
             }
         ]
 
-  }
-    // centerMode: true,
-    // centerPadding: '60px',
-    // slidesToShow: 3,
-    // responsive: [
-    //     {
-    //     breakpoint: 768,
-    //     settings: {
-    //         arrows: false,
-    //         centerMode: true,
-    //         centerPadding: '40px',
-    //         slidesToShow: 3
-    //     }
-    //     },
-    //     {
-    //     breakpoint: 480,
-    //     settings: {
-    //         arrows: false,
-    //         centerMode: true,
-    //         centerPadding: '40px',
-    //         slidesToShow: 1
-    //     }
-    //     }
-    // ]
-  );
+  });
 });
